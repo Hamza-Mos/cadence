5224f3a8009ccfeb98924772abc2ad200b8ef09a
Hamza Mostafa <hamza.oruba@gmail.com>
2025-01-04 13:42:07 -0500

    Hamza/update title (#3)
    * update title
    
    * update favicon
    
    * update description

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,8 +14,9 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Cadence: Microlearning made easy!",
+  description:
+    "Supercharge your microlearning with byte-sized texts using Cadence!",
 };
 
 export default function RootLayout({
