da66ccdcb8e3d46c0e7f4360c7c99179a2d9a021
Hamza Mostafa <hamza.oruba@gmail.com>
2025-01-05 14:17:19 -0500

    fix

--- app/auth/actions.ts
+++ app/auth/actions.ts
@@ -5,9 +5,10 @@ import { createClient } from "@/utils/supabase/server";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 import Stripe from "stripe";
+import { createServiceClient } from "@/utils/supabase/service-client";
 
 export async function signUpWithPhone(formData: FormData) {
-  const supabase = await createClient();
+  const supabase = createServiceClient();
   const areaCode = formData.get("countryCode") as string;
   const phoneNumber = formData.get("phoneNumber") as string;
   const firstName = formData.get("firstName") as string;
@@ -221,7 +222,7 @@ export async function verifyPhoneChange(formData: FormData) {
 }
 
 export async function signInWithPhone(formData: FormData) {
-  const supabase = await createClient();
+  const supabase = createServiceClient();
   const areaCode = formData.get("countryCode") as string;
   const phoneNumber = formData.get("phoneNumber") as string;
   const fullPhone = `+${areaCode}${phoneNumber.replace(/^0+/, "")}`;
